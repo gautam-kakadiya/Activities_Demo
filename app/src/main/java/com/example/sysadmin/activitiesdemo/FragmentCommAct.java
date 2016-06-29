252d9fd9e1567798b4beeebd3dd95e9e880fac74
gautam-kakadiya <gautam.d.kakadiya@gmail.com>
2016-06-29 18:44:58 +0530

    initialised FragmentCommAct

---
+++ app/src/main/java/com/example/sysadmin/activitiesdemo/FragmentCommAct.java
@@ -0,0 +1,14 @@
+package com.example.sysadmin.activitiesdemo;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FragmentCommAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment_comm);
+    }
+}
